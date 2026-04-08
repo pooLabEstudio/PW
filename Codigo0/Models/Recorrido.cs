64fd2e1cc16be60300d125c3888ca25fdcbbd368
pooLabEstudio <jorge.vera4@virginiogomez.cl>
2026-04-08 11:06:31 -0400

    Codigo version 0
    la solucion es juego
    JanVer

---
+++ Codigo0/Models/Recorrido.cs
@@ -0,0 +1,24 @@
+﻿// Models/Recorrido.cs
+namespace PW_Ejercicios.Models
+{
+    public class Recorrido
+    {
+        public double horas { get; set; } = 0;
+        public double minutos { get; set; } = 0;
+        public double segundos { get; set; } = 0;
+        public double centesimas { get; set; } = 0;
+        public double distancia { get; set; } = 0;
+
+        public double CalcularVelocidad()
+        {
+            double totalHoras = horas
+                              + (minutos / 60)
+                              + (segundos / 3600)
+                              + (centesimas / 360000);
+
+            double distKm = distancia / 1000;
+
+            return distKm / totalHoras; //retorna: velocidad = distancia en km / tiempo en horas
+        }
+    }
+}
